9dbcb302b14fc311999f357272daa6087bc33d5d
sgil0203 <surbaanigill@gmail.com>
2024-09-23 21:49:58 -0700

    Update Program6.java

--- Program6.java
+++ Program6.java
@@ -1,9 +1,28 @@
-//import stuff here!
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner (System.in);
+    System.out.println("Enter radius: ");
+    double radius = myObj.nextDouble();
+    double diameter = radius * 2;
+    int diameter1 = (int)(diameter * 1000);
+    double diameter2 = (double)diameter1 / 1000; 
+    final double pi = 3.142;
+    double circumference = pi * diameter;
+    int circumference1 = (int)(circumference * 1000);
+    double circumference2 = (double)circumference1 / 1000;
+    double area = pi * radius * radius;
+    int area1 = (int)(area * 1000);
+    double area2 = (double)area1 / 1000;
+    System.out.println("The radius is: " + radius);
+    System.out.println("The diameter is: " + diameter2);
+    System.out.println("The circumference is: " +       circumference2); 
+    System.out.println("The area is: " + area2);
+  }
+}
+//Enter radius: 3.172 
+//The radius is: 3.172
+//The diameter is: 6.344 
+//The circumference is 19.932 
+//The area is 31.613
 
-//Your code here
-
-//Paste console output below:
-/*
-
-
-*/
