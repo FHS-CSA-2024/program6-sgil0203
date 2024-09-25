5fcbe3e17fb80eca20d2c40295476a1f6e5526f3
Benjamin Johnson <bjohnson@fcusd.org>
2024-09-25 08:02:26 -0700

    Update Program6.java
    Need to have the class name match the file name

--- Program6.java
+++ Program6.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class Program6 {
   public static void main(String[] args) {
     Scanner myObj = new Scanner (System.in);
     System.out.println("Enter radius: ");
